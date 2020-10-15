e437d39e0ac38df6b27a279045df5106b2cb5916
ThomasVECAM <17030@ecam.be>
2020-10-15 17:20:09 +0200

    Exercice 2

--- exercises/exo2/reader.go
+++ exercises/exo2/reader.go
@@ -19,3 +19,15 @@ func main() {
 // Implement a type that satisfies the io.Reader interface and reads from another io.Reader,
 // modifying the stream by removing the spaces.
 // Expected output: "Helloworld!"
+
+func(reader spaceEraser) Read(buffer []byte) (int,error){
+	n, err := reader.r.Read(buffer)
+	nbCaract :=0
+	for i := 0; i < n ; i++{
+		if buffer[i] != 32 {
+			buffer[nbCaract] = buffer[i] //le buffer, ne va pas changer de taille mais les éléments dans le buffer vont changer de place.
+			nbCaract++
+		}
+	}
+	return nbCaract, err
+}
