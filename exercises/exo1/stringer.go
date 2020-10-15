d4ad127ab4a88b76f44bc2e6afd4d63ba14e3be1
ThomasVECAM <17030@ecam.be>
2020-10-15 16:08:48 +0200

    Exercice 1

--- exercises/exo1/stringer.go
+++ exercises/exo1/stringer.go
@@ -1,19 +1,36 @@
 package main
-
 import "fmt"
 
-// Implement types Rectangle, Circle and Shape
+// IN THIS CODE, I CONSIDERED THAT THE DIFFERENT LENGTHS ARE OF TYPE INT.
+
+type Shape interface{
+	String() string
+}
+
+type Rectangle struct {
+	X int
+	Y int
+}
+
+type Circle struct {
+	R int
+}
+
+func(rectangle Rectangle) String() string{
+	return fmt.Sprintf("Square of width %d and length %d",rectangle.X,rectangle.Y)
+}
+
+func(circle Circle) String() string{
+	return fmt.Sprintf("Circle of radius %d",circle.R)
+}
 
 func main() {
 	r := &Rectangle{2, 3}
-	c := &Circle{5}
+    c := &Circle{5}
 
-	shapes := []Shape{r, c}
+    shapes := []Shape{r, c}
 
-	for _, s := range shapes {
-		fmt.Println(s)
-		// Expected output:
-		// Square of width 2 and length 3
-		// Circle of radius 5
+    for _, s := range shapes {
+        fmt.Println(s)
 	}
 }
